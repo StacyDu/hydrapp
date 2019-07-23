3558713c9d3ad5e1a1a5366abf9320d0e59d1f56
StacyDu <dudinaanastasiia@gmail.com>
2019-07-23 20:03:25 +0200

    New starter kit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
 "use strict";
 
-import moment from 'moment';
+// service worker registration - remove if you're not going to use it
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+// place your code below
+
+
+console.log(`Hello world!`);
+
+
